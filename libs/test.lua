a323aaf5abaccce5bc78daaa6d79dc1bc6d70b03
jinjiazhang <jinjiazh@qq.com>
2017-11-16 15:57:47 +0800

    update test code

--- libs/test.lua
+++ libs/test.lua
@@ -2,17 +2,17 @@ require "protolua"
 proto.parse("person.proto")
 
 local player = {
-	id = 10001,
-	name = "jinjiazh",
-	email = "jinjiazh@qq.com",
-	phones = {
-		{number = "1818864xxxx", type = 1},
-		{number = "1868200xxxx", type = 2},
-	}
+    name = "jinjiazh",
+    id = 10001,
+    email = "jinjiazh@qq.com",
+    phones = {
+        {number = "1818864xxxx", type = 1},
+        {number = "1868200xxxx", type = 2},
+    }
 }
 
 local data = proto.encode("Person", player)
-local copy = proto.decode("Person", data)
+local clone = proto.decode("Person", data)
 
 local data = proto.pack("Person", player.id, player.name, player.email, player.phones)
 local id, name, email, phones = proto.unpack("Person", data)
